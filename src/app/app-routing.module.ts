6e29db43155297b7e8857b45563c55f7f4e31b11
Carlos Daniel Espinoza Huerta <espinozahuerta98@gmail.com>
2021-03-09 18:31:37 -0500

    loginApp

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './guards/auth.guard';
 
-const routes: Routes = [];
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegistroComponent } from './pages/registro/registro.component';
+
+
+const routes: Routes = [
+  { path: 'home'    , component: HomeComponent, canActivate:[AuthGuard]},
+  { path: 'registro', component: RegistroComponent },
+  { path: 'login'   , component: LoginComponent },
+  { path: '**', redirectTo: 'registro' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
